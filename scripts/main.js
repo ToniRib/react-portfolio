bf01b7ddad764add79035585a7ac0c8463da2c76
ToniRib <tonimarierib@gmail.com>
2015-12-29 21:24:46 -0700

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+// Require React
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// Require CSS Transition Group
+var CSSTransitionGroup = require('react-addons-css-transition-group');
+var ReactRouter = require('react-router');
+
+// Require ReactRouter
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Navigation = ReactRouter.Navigation;
+var History = ReactRouter.History;
+var createBrowserHistory = require('history/lib/createBrowserHistory');
+
+// Require the helpers
+var h = require('./helpers');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div>Hello!</div>
+    )
+  }
+})
+
+var routes = (
+  <Router history={createBrowserHistory()}>
+    <Route path="/" component={App} />
+  </Router>
+)
+
+// Render the application
+ReactDOM.render(routes, document.querySelector('#main'));
